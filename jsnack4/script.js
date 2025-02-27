661cc7eed09c78f7d5091e34e57ee50d0eee6fea
vmqxdv <vquintilio@gmail.com>
2025-02-27 12:29:43 +0100

    jsnack4

---
+++ jsnack4/script.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Crea un array vuoto.
+ * Chiedi per 6 volte all’utente di inserire un numero,
+ * se è dispari inseriscilo nell’array
+ * 
+ * 
+ */
+
+const oddNums = [];
+for (let i = 0; i <= 10; i++) {
+  const num = Number(prompt('Inserisci un numero casuale'));
+  if (num % 2) oddNums.push(num);
+};
+
+alert(`I numeri dispari che hai inserito erano: ${oddNums.join(', ')}`);
