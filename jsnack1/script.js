77dd0f242b31f899d0c3ea9d9c287c7d6c3de655
vmqxdv <vquintilio@gmail.com>
2025-02-27 12:43:11 +0100

    refractor: Aggiunti commenti

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -6,9 +6,14 @@
  * 
  */
 
+// Chiedo all'utente un primo numero casuale
 const firstNum = Number(prompt('Scrivi un numero casuale'));
+
+// Chiedo all'utente un secondo numero casuale
 const SecondNum = Number(prompt('Scrivi un altro numero casuale'));
 
+// Controllo quale dei 2 numeri è più grande
 let biggestNum = firstNum > SecondNum ? firstNum : SecondNum;
 
+// Notifico l'utente quali dei 2 numeri inserito è più grande
 alert(`Il numero più grande è: ${biggestNum}`);
