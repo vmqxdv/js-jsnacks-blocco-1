932763e9bfb7b3751a03cf4f9a1e6141a419a734
vmqxdv <vquintilio@gmail.com>
2025-02-27 12:41:41 +0100

    refractor: Aggiunti commenti, e cambiato da let a const

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,9 +6,14 @@
  * 
  */
 
+// Chiedo all'utente una prima parola casuale
 const firstWord = prompt('Scrivi una parola casuale');
+
+// Chiedo all'utente una seconda parola casuale
 const SecondWord= prompt('Scrivi un\'altra parola casuale');
 
-let biggestWord = firstWord.length > SecondWord.length ? firstWord : SecondWord;
+// controllo quale delle 2 parole inserite è più lunga
+const biggestWord = firstWord.length > SecondWord.length ? firstWord : SecondWord;
 
+// Notifico l'utente quali delle 2 parole inserite è più lunga
 alert(`la parola più lunga è: ${biggestWord} (${biggestWord.length})`);
