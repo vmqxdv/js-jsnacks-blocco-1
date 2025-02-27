9d5ad89c79f8b3bad79c6420797caccb887a4755
vmqxdv <vquintilio@gmail.com>
2025-02-27 12:37:55 +0100

    refractor: Aggiunto commenti

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -7,10 +7,19 @@
  * 
  */
 
+
+// Creo un array vuoto dove salvare eventuali numeri dispari
 const oddNums = [];
+
+// Avvio un loop per chiedere N volte un numero
 for (let i = 0; i <= 6; i++) {
+  
+  // Trasformo l'input da stringa a Number per poter controllare dopo se è dispari 
   const num = Number(prompt('Inserisci un numero casuale'));
+
+  // Controllo se il numero è dispari, se si lo aggiungo all'array creato precedentemente
   if (num % 2) oddNums.push(num);
 };
 
+// Notifico l'utente quali numeri dispari ha inserito
 alert(`I numeri dispari che hai inserito erano: ${oddNums.join(', ')}`);
