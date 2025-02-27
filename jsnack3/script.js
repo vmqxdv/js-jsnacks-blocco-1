cf59ea7d8a410ec1b85e0649431e5e179bbbc952
vmqxdv <vquintilio@gmail.com>
2025-02-27 12:39:50 +0100

    refractor: Aggiunti commenti

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -6,10 +6,18 @@
  * 
  */
 
+// Dichiato una variabile `let` con valore 0
 let total = 0;
+
+// Avvio un loop per chiedere N volte un numero
 for (let i = 0; i <= 10; i++) {
+
+  // Trasformo l'input da stringa a Number per poter controllare dopo se è dispari 
   const numToAdd = Number(prompt('Inserisci un numero casuale'));
+
+  // Addiziono l'ultimo numero inserito al totale precedente
   total += numToAdd;
 };
 
+// Notifico l'utente della somma dei numeri che ha inserito
 alert(`La somma di tutti i numeri inseriti è: ${total}`);
